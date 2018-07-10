ee86371757d077e1bd6202a124b21a16831dc2f1
Javier <moubayyed47@hotmail.com>
2018-07-10 20:05:27 +0200

    heroku need files

--- libro/settings.py
+++ libro/settings.py
@@ -131,5 +131,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
