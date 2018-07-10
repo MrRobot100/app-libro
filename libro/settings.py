f8225d33ea45cf24a3aa19f020b524591d1961bd
Javier <moubayyed47@hotmail.com>
2018-07-10 20:39:31 +0200

    database conenct

--- libro/settings.py
+++ libro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
