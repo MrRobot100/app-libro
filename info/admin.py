75b93530c0cd9d94b7fd6f3895339724bebdfeca
Javier <moubayyed47@hotmail.com>
2019-11-24 09:45:37 +0100

    styles check

--- info/admin.py
+++ info/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Informacion, Usuarios, Front
 # Register your models here.
 
-
 admin.site.register(Informacion)
 admin.site.register(Usuarios)
 admin.site.register(Front)
--- info/forms.py
+++ info/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class Correo(forms.Form):
+
     correo = forms.EmailField(label='user_mail', required=True)
+
     nombre = forms.CharField(label='nombre', max_length=200, required=True)
+    
     apellido = forms.CharField(label='nombre', max_length=200, required=True)
